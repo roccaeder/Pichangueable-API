a327754a5a2934a680c8345c14e06824c7c1614b
eder <edra1791@gmail.com>
2021-11-09 17:18:41 +0000

    add parameter for the API and exec bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post "login", to: "sessions#create"
-  post "logout", to: "sessions#destroy"
+  # post "login", to: "sessions#create"
+  # post "logout", to: "sessions#destroy"
 end
