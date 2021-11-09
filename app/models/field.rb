4965cf3e55e16cf99e8889376f071f22b0a767c7
natlines1492 <natlines14@gmail.com>
2021-11-09 16:16:43 +0000

    Add associations to models

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,4 +1,7 @@
 class Field < ApplicationRecord
-  belongs_to :user
-  belongs_to :ubication
+  belongs_to :user # required: true
+  belongs_to :ubication # required: true
+  has_many :reviews, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+  # has_many :field_images, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :omniauthable, :validatable,
+         omniauth_providers: %i[google_oauth2]
+
+  has_many :reservations, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :fields, dependent: :destroy
+
+  enum role: {
+    user: 0,
+    admin: 1
+  }
 end
