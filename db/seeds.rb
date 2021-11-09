5d67b496775f9d58870f583e2fa5c51a06169fdc
Anitazc13 <anamaria.zc.13@gmail.com>
2021-11-09 12:11:49 -0500

    Add the ubication and users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.destroy_all
+
+
+puts "Start seeding Users"
+  4.times do 
+    users = [
+        { name: 'Natz', email: 'natz@mail.com', phone: '975318642', rol: 1 },
+        { name: 'Aldo', email: 'aldo@mail.com', phone: '975335642', rol: 1 },
+        { name: 'Ana', email: 'anita@mail.com', phone: '971218642', rol: 0 },
+        { name: 'Rodrigo', email: 'rodri@mail.com', phone: '975312332', rol: 0 },
+        { name: 'Edder', email: 'eder@mail.com', phone: '911112222', rol: 0 },
+      ]
+  User.create(users)
+end
+puts "End seeding Users"
+
+puts "Start seeding Ubication"
+  9.times do 
+    users = [
+        { district: 'El Agustino', department: 'Lima'},
+        { district: 'S.J.L.', department: 'Lima'},
+        { district: 'S.J.M.', department: 'Lima'},
+        { district: 'Breña', department: 'Lima'},
+        { district: 'Miraflores', department: 'Lima'},
+        { district: 'La Victoria', department: 'Lima'},
+        { district: 'Surquillo', department: 'Lima'},
+        { district: 'Barranco', department: 'Lima'},
+        { district: 'Surco', department: 'Lima'},
+        { district: 'La Molina', department: 'Lima'}
+      ]
+  User.create(users)
+end
+puts "End seeding Users"
