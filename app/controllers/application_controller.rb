e7ceb1938dd9a24f96ea2b0ea8a7e4c7cac0f514
Aldo <aldocanales91@gmail.com>
2021-11-09 17:01:48 +0000

    Adding the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   before_action :require_login!
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
@@ -11,5 +11,4 @@ class User < ApplicationRecord
     user: 0,
     admin: 1
   }
-
 end
