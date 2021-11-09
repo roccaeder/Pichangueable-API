fb9ad8f2cea75dbc46686c36af52b4b0ea0d7137
rdrg0 <rodrigo.arriaran@gmail.com>
2021-11-09 16:59:48 +0000

    validate reservation and field

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,4 +1,12 @@
 class Field < ApplicationRecord
   belongs_to :user
   belongs_to :ubication
+
+  validates :field_type, presence: true
+  validates :summary, length: { maximum: 140 }
+  validates :user_id, presence: true
+  validates :price_hour, presence: true
+  validates :ubication_id, presence: true
+
+  validates :published_at, presence: true
 end
