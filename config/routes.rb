3d22073ca9c1e8f33c53ccd85da8b02bdc24c304
Anitazc13 <anamaria.zc.13@gmail.com>
2021-11-09 11:10:33 -0500

    Add the users and email for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :reviews
-  resources :reservations
-  resources :fields
-  resources :ubications
-  resources :users
+
+    devise_for :users
+    resources :reviews
+    resources :reservations
+    resources :fields
+    resources :ubications
+    resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
