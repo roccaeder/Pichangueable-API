b3be3f0a05fa95f53fb28d5f2d0bf4e785672166
rdrg0 <rodrigo.arriaran@gmail.com>
2021-11-10 21:12:42 +0000

    add ubications controller

--- app/controllers/ubications_controller.rb
+++ app/controllers/ubications_controller.rb
@@ -1,2 +1,12 @@
 class UbicationsController < ApplicationController
+  def index
+    @ubications = Ubication.all
+    render json: @ubications
+  end
+
+  private
+
+  def ubication_params
+    params.permit(:district, :department)
+  end
 end
