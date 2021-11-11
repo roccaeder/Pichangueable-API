ca9f0b965e17a31b3532a8e291f7b4ba266d9a9b
eder <edra1791@gmail.com>
2021-11-11 18:57:43 +0000

    add field controller and review ucations

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,7 +1,6 @@
 class FieldsController < ApplicationController
   # before_action :require_login % i[index]
-  before_action :require_login, except: %i[index]
-
+  before_action :require_login
   def index
     @fields = Field.all
     render json: @fields
@@ -10,16 +9,21 @@ class FieldsController < ApplicationController
   # new field
   def create
     @field = Field.new(field_params)
+    @field.user_id = current_user.id
     if @field.save
       render json: @field
     else
       render json: @field.errors, status: :unprocessable_entity
     end
+    # render json: params
   end
 
   def show
-    field = Field.where(id: params[:id])
-    render json: field
+    field = Field.find(params[:id])
+    # pp field.images.map{ |image| ({image: url_for(image)})}
+    render json: field.as_json.merge({ images: field.images.map do |image|
+                                                 { image_url: url_for(image) }
+                                               end })
   end
 
   def update
@@ -38,6 +42,7 @@ class FieldsController < ApplicationController
   private
 
   def field_params
-    params.permit(:name, :summary, :field_type, :sports_type, :address, :capacity, :price_hour)
+    params.permit(:name, :summary, :field_type, :sport_type, :address, :capacity, :price_hour,
+                  :images, :ubication_id, :published_at)
   end
 end
